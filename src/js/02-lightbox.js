20aa5666dd86758186f6f966c572face955f93ec
MarharytaBoichenko <boichenkomv@gmail.com>
2021-09-01 23:21:26 +0300

    add

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -16,16 +16,9 @@ galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
 
 function  createMarkup(gallery){
     return gallery.map(({ preview, original, description }) =>
-        `<div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src=${preview}
-      data-source=${original}
-      alt="${description}"
-    />
-  </a>
-</div>`
+        `<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`
     ).join('');
 
 };
@@ -33,13 +26,25 @@ console.log(galleryMarkup);
 
 galleryList.addEventListener("click", onModalClick);
 
-function onModalClick() {
-    e.preventDefault();
-    if (e.target.nodeName !== IMG) {
-        return
-    } 
+// function onModalClick() {
+//     e.preventDefault();
+//     if (e.target.nodeName !== IMG) {
+//         return
+//     } 
 
-    let lightbox = new SimpleLightbox('.galleryList a', {
+//     let lightbox = new SimpleLightbox('.galleryList a', {
+// 	animationSpeed: 500,
+// 	fadeSpeed: 500,
+// 	captions: true,
+// 	captionType: 'attr',
+// 	captionsData: 'alt',
+// 	captionPosition: 'bottom',
+// 	captionDelay: 250,
+// });
+    
+// };
+
+   let lightbox = new SimpleLightbox('.gallery a', {
 	animationSpeed: 500,
 	fadeSpeed: 500,
 	captions: true,
@@ -48,8 +53,9 @@ function onModalClick() {
 	captionPosition: 'bottom',
 	captionDelay: 250,
 });
-    
-};
+
 lightbox.on(() => {
 	lightbox.next();
 });
+
+
